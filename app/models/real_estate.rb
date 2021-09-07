8505f785cacb315f5e02d234a8237d406fd2ef90
Septentrion50 <erwan56lecorre@gmail.com>
2021-09-07 18:09:31 +0200

    Image processing

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,16 +1,36 @@
 class RealEstate < ApplicationRecord
+
+  include ActiveModel::Serializers::JSON
+
   belongs_to :user
   belongs_to :category
 
   has_many_attached :images
 
-  def get_images_url
-   return unless self.images.attached?
-   images = []
-   self.images.each do |i|
-    images << Rails.application.routes.url_helpers.rails_blob_path(i, only_path: true)
-   end
-   return images
+  def attributes
+    {
+      'id': nil,
+      'title': nil,
+      'description': nil,
+      'location': nil,
+      'address': nil,
+      'price': nil,
+      'category': nil,
+      'user': nil,
+      'updated_at': nil,
+      'created_at': nil,
+      'images_url': nil
+    }
+  end
+
+  def images_url
+    images = []
+    self.images.each do |image|
+      images << Rails.application.routes.url_helpers.rails_representation_url(
+        image.variant(resize_to_limit: [200, 200]).processed, only_path: true
+      )
+    end
+    return images
   end
 
 end
