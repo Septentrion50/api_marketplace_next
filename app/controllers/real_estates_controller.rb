a14b9e8795a76446dc0894709c0f94187ae898a9
Septentrion50 <erwan56lecorre@gmail.com>
2021-09-08 09:20:51 +0200

    Users can see the real_estates pages unlogged

--- app/controllers/real_estates_controller.rb
+++ app/controllers/real_estates_controller.rb
@@ -1,6 +1,6 @@
 class RealEstatesController < ApplicationController
   before_action :set_real_estate, only: [:show, :update, :destroy]
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
   # GET /real_estates
